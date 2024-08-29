2211ee9704e265e6b2743b73487eff68112f5fb8
ahmad <mofijur.rahman@technonext.com>
2024-08-29 17:44:56 +0600

    feat: add the new form component the input text

---
+++ components/form/InputText.tsx
@@ -0,0 +1,50 @@
+import React, { Fragment } from "react";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+
+// =====> Types & Interfaces <===== //
+interface Props<TFieldValues extends FieldValues> {
+  name: Path<TFieldValues>;
+  control: Control<TFieldValues, any>;
+  label?: string;
+  type?: React.HTMLInputTypeAttribute;
+  defaultValue?: any;
+  isRequired?: boolean;
+}
+
+// =======> Main Function <======= //
+const InputText = <TFieldValues extends FieldValues>({
+  name,
+  label,
+  control,
+  type = "text",
+  defaultValue,
+  isRequired = false,
+}: Props<TFieldValues>) => {
+  return (
+    <div className="mb-3">
+      {label && (
+        <label htmlFor={name} className="block">
+          {label}
+          {isRequired && <span className="text-error"> *</span>}
+        </label>
+      )}
+      <Controller
+        name={name}
+        control={control}
+        defaultValue={defaultValue}
+        render={({ field, fieldState: { error } }) => (
+          <Fragment>
+            <input
+              {...field}
+              type={type}
+              className="w-full rounded-lg border border-gray-300 p-2 focus:border-primary focus:outline-none"
+            />
+            {error && <p className="text-error">{error?.message}</p>}
+          </Fragment>
+        )}
+      />
+    </div>
+  );
+};
+
+export default InputText;
