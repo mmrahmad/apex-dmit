4d7df08b41e1d857df6b9568437a75a1b0681fc7
ahmad <mofijur.rahman@technonext.com>
2024-08-29 19:33:59 +0600

    refactor: update the login page with component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
-import LoginForm from "#/components/login/LoginForm";
+import LoginPage from "#/components/login";
 
 export default function Home() {
-  return (
-    <main className="container">
-      <div className="grid h-full min-h-screen grid-cols-2 items-center justify-center">
-        <div>Test</div>
-        <div>
-          <LoginForm />
-        </div>
-      </div>
-    </main>
-  );
+  return <LoginPage />;
 }
