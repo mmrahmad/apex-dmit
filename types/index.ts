e4ea5e96d94f952eb1bed167f0c86b2a26fea595
ahmad <mofijur.rahman@technonext.com>
2024-08-29 19:32:35 +0600

    feat: add testimonial type

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,20 @@
+import { number } from "yup";
+
+// ----------------------- Login Page ------------------ //
 export type LoginFormValues = {
   email: string;
   password: string;
+};
+
+type authorType = {
+  name: string;
+  designation: string;
+  avatar: string;
+};
+
+export type TestimonialType = {
+  id: number;
+  rating: number;
+  comment: string;
+  author: authorType;
 };
